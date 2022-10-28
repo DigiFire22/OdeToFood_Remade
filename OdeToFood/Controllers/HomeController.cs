b257f9e515cd942475f296b878528f8acca59c19
opilane <opilane@HP-CZC6297B8V>
2022-10-28 09:00:48 +0300

    Fixed Start up and home Controller

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -15,52 +15,53 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+        public HomeController(ApplicationDbContext dbContext)
         {
-            _logger = logger;
             _db = dbContext;
         }
 
-        public ActionResult Autocomplete(string term)
+
+        public IActionResult Autocomplete(string term)
         {
-            var model = _db.Restaurants.Where(r => r.Name.StartsWith(term)).Take(10).Select(r => new
-            {
-                label = r.Name
-            });
+            var model = _db.Restaurants
+                .Where(r => r.Name.StartsWith(term))
+                .Take(10)
+                .Select(r => new { label = r.Name });
             return Json(model);
         }
 
+
         public IActionResult Index(string searchTerm = null, int page = 1)
         {
-            var model = _db.Restaurants.OrderByDescending(r => r.Review.Average(review => review.Rating))
-            .Where(r => searchTerm == null || r.Name.Contains(searchTerm))
-            .Select(r => new RestaurantListViewModel
-            {
-            Id = r.Id,
-            Name = r.Name,
-            City = r.City,
-            Country = r.Country,
-            CountOfReviews = r.Review.Count
-            }).ToPagedList(page, 10);
+            var model = _db.Restaurants
+                .OrderByDescending(r => r.Review.Average(review => review.Rating))
+                .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+                .Select(r => new RestaurantListViewModel
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    City = r.City,
+                    Country = r.Country,
+                    CountOfReviews = r.Review.Count()
+                }
+                ).ToPagedList(page, 10);
 
             if (Request.IsAjaxRequest())
             {
                 return PartialView("_Restaurants", model);
             }
 
+
             return View(model);
         }
 
         public IActionResult About()
         {
-            var model = new AboutModel()
-            {
-                Name = "Sten Christjan Uesson",
-                Location = "Tallinn"
-            };
+            var model = new AboutModel();
+            model.Name = "Sten Christjan Uesson";
+            model.Location = "Tallinn";
             return View(model);
         }
 
@@ -76,6 +77,10 @@ namespace OdeToFood.Controllers
         }
         protected override void Dispose(bool disposing)
         {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
             base.Dispose(disposing);
         }
     }
